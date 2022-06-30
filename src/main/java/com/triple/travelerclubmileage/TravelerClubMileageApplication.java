abaf6e89b7e1f0659faaf2f2d2fa332adba54e93
seoeunbae <enyoupark@gmail.com>
2022-06-30 11:36:42 +0900

    initiallize project - release branch

---
+++ src/main/java/com/triple/travelerclubmileage/TravelerClubMileageApplication.java
@@ -0,0 +1,13 @@
+package com.triple.travelerclubmileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelerClubMileageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelerClubMileageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triple/travelerclubmileage/TravelerClubMileageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.travelerclubmileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerClubMileageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
